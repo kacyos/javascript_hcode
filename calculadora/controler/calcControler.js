3649d983076e8c4dd0dc54694116ea8253906d47
Cacio de Castro <cacio.castro@gmail.com>
2020-05-20 22:05:16 -0300

    alterações de aula

--- calculadora/controler/calcControler.js
+++ calculadora/controler/calcControler.js
@@ -1,7 +1,57 @@
 class CalcControler {
-    constructor(){
-        this._displayCalc = "0";
-        this._dataAtual;
-        var cas = 999;
-    }
+  constructor() {
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this.setDisplayDateTimne();
+    setInterval(() => {
+      this.setDisplayDateTimne();
+    }, 1000);
+  }
+
+  initButtonsEvents(){
+      let buttons = document.querySelectorAll("#buttons > g, #oarts > g");
+  }
+
+  setDisplayDateTimne() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {day: "2-digit", month: "long", year: "numeric"});
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return (this._timeEl.innerHTML = value);
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    return (this._dateEl.innerHTML = value);
+  }
+  get displayCalc() {
+    return this._displayCalcEl.innerHTML;
+  }
+
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
+  }
+
+  get currentDate() {
+    return new Date();
+  }
+
+  set currentDate(date) {
+    this._currentDate = date;
+  }
 }
