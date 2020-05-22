2a900882a8601abb8d6e794c57bd8e07f737a724
Allan Souza <Allan Souza@11rirj.net>
2020-05-22 13:57:58 -0300

    alterações de aula

--- calculadora/controler/calcControler.js
+++ calculadora/controler/calcControler.js
@@ -31,8 +31,55 @@ class CalcControler {
     this._operation.pop();
   }
 
-  addOperation(value) {
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+    return ["+", "-", "*", "/", "%"].indexOf(value) > -1;
+  }
+
+  pushOperation(value) {
     this._operation.push(value);
+
+    if (this._operation.length > 3) {
+      this.calc();
+    }
+  }
+
+  calc() {
+    let last = this._operation.pop();
+
+    let result = eval(this._operation.join(""));
+
+    this._operation = [result, last];
+  }
+
+  setLastNumberToDisplay() {}
+
+  addOperation(value) {
+    if (isNaN(this.getLastOperation())) {
+      if (this.isOperator(value)) {
+        this.setLastOperation(value);
+      } else if (isNaN(value)) {
+        console.log("outra coisa");
+      } else {
+        this.pushOperation(value);
+      }
+    } else {
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+        this.setLastOperation(parseInt(newValue));
+        this.setLastNumberToDisplay();
+      }
+    }
+
     console.log(this._operation);
   }
 
@@ -49,19 +96,28 @@ class CalcControler {
         this.clearEntry();
         break;
 
+      case "ponto":
+        this.addOperation(".");
+        break;
+
       case "soma":
+        this.addOperation("+");
         break;
 
       case "subtracao":
+        this.addOperation("-");
         break;
 
       case "divisao":
+        this.addOperation("/");
         break;
 
       case "multiplicacao":
+        this.addOperation("*");
         break;
 
       case "porcento":
+        this.addOperation("%");
         break;
 
       case "igual":
